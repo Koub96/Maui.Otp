8851ee2d88c56d212afaa913d8651532ad74c751
Konstantinos Koumpanakis <kko@entersoft.gr>
2026-05-17 18:37:26 +0300

    deleted old sample files

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
 
-[assembly: XmlnsDefinition("https://github.com/Koub96/Maui.Otp", "Maui.Otp")]
-[assembly: XmlnsPrefix("https://github.com/Koub96/Maui.Otp", "otp")]
+[assembly: XmlnsDefinition("https://github.com/KKoub96/Maui.Otp", "Maui.Otp")]
+[assembly: XmlnsPrefix("https://github.com/KKoub96/Maui.Otp", "otp")]
